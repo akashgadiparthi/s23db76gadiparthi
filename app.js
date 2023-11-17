547f8db61bd59413c35b970ec0b8d3dc321accab
akashgadiparthi <143130459+akashgadiparthi@users.noreply.github.com>
2023-11-17 13:14:37 -0600

    passport code and update buttons

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+function(username, password, done) {
+Account.findOne({ username: username })
+.then(function (user){
+if (err) { return done(err); }
+if (!user) {
+return done(null, false, { message: 'Incorrect username.' });
+}
+if (!user.validPassword(password)) {
+return done(null, false, { message: 'Incorrect password.' });
+}
+return done(null, user);
+})
+.catch(function(err){
+return done(err)
+})
+})
+)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,15 +50,15 @@ console.log("Connection to DB succeeded")});
 async function recreateDB(){
 // Delete everything
 await Fox.deleteMany();
-let instance1 = new Fox({Fox_color:"green", Fox_breed:'Domesticated silver fox',Fox_price:1100});
+let instance1 = new Fox({Fox_color:'green', Fox_breed:'Domesticated silver fox',Fox_price:1100});
 instance1.save().then(doc=>{
 console.log("First object saved")}
 )
-let instance2 = new Fox({Fox_color:"red", Fox_breed:'Vulpes vulpes fulva',Fox_price:13500});
+let instance2 = new Fox({Fox_color:'red', Fox_breed:'Vulpes vulpes fulva',Fox_price:13500});
 instance2.save().then(doc=>{
 console.log("Second object saved")}
 )
-let instance3 = new Fox({Fox_color:"yellow", Fox_breed:'English Foxhound',Fox_price:9000});
+let instance3 = new Fox({Fox_color:'yellow', Fox_breed:'English Foxhound',Fox_price:9000});
 instance3.save().then(doc=>{
 console.log("Third object saved")}
 )
@@ -56,6 +79,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -64,6 +94,14 @@ app.use('/Fox', FoxRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource',resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 
 
--- routes/resource.js
+++ routes/resource.js
@@ -19,11 +19,3 @@ router.get('/Fox/:id', Fox_controller.Fox_detail);
 router.get('/Fox', Fox_controller.Fox_list);
 module.exports = router;
 // API for our resources
-exports.api = function(req, res) {
-res.write('[');
-res.write('{"resource":"Fox", ');
-res.write(' "verbs":["GET","PUT", "DELETE"] ');
-res.write('}');
-res.write(']')
-res.send();
-};
