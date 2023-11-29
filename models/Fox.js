5ed297bcb35da0751c985c76d3cfeade25907446
akashgadiparthi <143130459+akashgadiparthi@users.noreply.github.com>
2023-11-29 17:14:13 -0600

    added bonus

--- models/Fox.js
+++ models/Fox.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const FoxSchema = mongoose.Schema({
 Fox_color: {type:String},
-Fox_breed: {type:String},
+Fox_breed: {type:String,minLength:10},
 Fox_price: {type:Number,required:true,min:4,max:50000}
 })
 module.exports = mongoose.model("Fox",FoxSchema)
--- routes/Fox.js
+++ routes/Fox.js
@@ -23,9 +23,9 @@ router.get('/', Fox_controlers.Fox_view_all_Page );
 /* GET detail Fox page */
 router.get('/detail', Fox_controlers.Fox_view_one_Page);
 /* GET create Fox page */
-router.get('/create', Fox_controlers.Fox_create_Page);
+router.get('/create',secured, Fox_controlers.Fox_create_Page);
 /* GET create update page */
 router.get('/update',secured,Fox_controlers.Fox_update_Page);
 /* GET delete Fox page */
-router.get('/delete', Fox_controlers.Fox_delete_Page);
+router.get('/delete',secured, Fox_controlers.Fox_delete_Page);
 module.exports = router;
